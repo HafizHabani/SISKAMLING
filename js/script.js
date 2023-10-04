492844ee11f4f0d623ccc88d0c1e8c6d1aa5989e
HafizHabani <hafizhbni@gmail.com>
2023-10-05 01:45:45 +0700

    Update 5 October 2023

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var loginpetugas = document.getElementById('login-petugas');
+
+window.onclick = function(event) {
+    if (event.target == loginpetugas) {
+        loginpetugas.style.display = "none";
+    }
+}
